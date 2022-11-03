3c83553106fe3a0d6c592dd950debc2861e6f3cb
sijo mathew <50240084+sijobnglr@users.noreply.github.com>
2022-11-03 18:06:55 +0530

    Add files via upload

---
+++ RTC_1307_CONFIG.sh
@@ -0,0 +1,3 @@
+!# /etc/bin/bash
+
+ sudo nano /boot/config.txt && sudo apt-get -y remove fake-hwclock && sudo update-rc.d -f fake-hwclock remove && sudo systemctl disable fake-hwclock && sudo nano /lib/udev/hwclock-set
