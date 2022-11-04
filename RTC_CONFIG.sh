6a1acc43204c594c3e5b42792b942fe992f398a6
sijo mathew <50240084+sijobnglr@users.noreply.github.com>
2022-11-04 12:03:31 +0530

    Update RTC_CONFIG.sh

--- RTC_CONFIG.sh
+++ RTC_CONFIG.sh
@@ -1,3 +1,3 @@
 !# /etc/bin/bash
 
- sudo nano /boot/config.txt && sudo apt-get -y remove fake-hwclock && sudo update-rc.d -f fake-hwclock remove && sudo systemctl disable fake-hwclock && sudo raspi-config && sudo apt install i2c-tools && i2cdetect -y 1
+ sudo nano /boot/config.txt && sudo apt-get -y remove fake-hwclock && sudo update-rc.d -f fake-hwclock remove && sudo systemctl disable fake-hwclock && sudo raspi-config && sudo apt install i2c-tools && i2cdetect -y 1 && sudo reboot
