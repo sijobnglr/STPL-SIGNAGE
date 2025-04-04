a8ba9d34f522e15314d85d4e238b196051d5c1eb
sijo mathew <50240084+sijobnglr@users.noreply.github.com>
2025-04-04 13:50:29 +0530

    Update rtc2.sh

--- rtc2.sh
+++ rtc2.sh
@@ -1,3 +1,3 @@
 !# /etc/bin/bash
 
-i2cdetect -y 1 && sudo apt-get -y remove fake-hwclock && sudo update-rc.d -f fake-hwclock remove && sudo systemctl disable fake-hwclock && sudo nano /lib/udev/hwclock-set && date && sudo hwclock -r && sudo hwclock -w && sudo hwclock -w -l && sudo hwclock -s -l && sudo raspi-config && timedatectl && sudo hwclock -w -l && sudo hwclock -s -l && timedatectl
+i2cdetect -y 1 && sudo apt-get -y remove fake-hwclock && sudo update-rc.d -f fake-hwclock remove && sudo systemctl disable fake-hwclock && sudo nano /lib/udev/hwclock-set && date && sudo hwclock -r && sudo hwclock -w && sudo hwclock -w -l && sudo hwclock -s -l && sudo raspi-config && timedatectl && sudo hwclock -w -l && sudo hwclock -s -l && timedatectl && sudo shutdwon -h
